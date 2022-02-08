4389bd50187b351b367574656ee7d1a344aa2042
Mercurii <Mercurii7@users.noreply.github.com>
2022-02-08 12:38:04 +0800

    Init

---
+++ contracts/MyArrays/src/MyArraysContract.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace MyArrays
+{
+    [DisplayName("YourName.MyArraysContract")]
+    [ManifestExtra("Author", "Your name")]
+    [ManifestExtra("Email", "your@address.invalid")]
+    [ManifestExtra("Description", "Describe your contract...")]
+    public class MyArraysContract : SmartContract
+    {
+        private static StorageMap ContractStorage => new StorageMap(Storage.CurrentContext, "MyArraysContract");
+        private static StorageMap ContractMetadata => new StorageMap(Storage.CurrentContext, "Metadata");
+
+        private static Transaction Tx => (Transaction) Runtime.ScriptContainer;
+
+        [DisplayName("NumberChanged")]
+        public static event Action<UInt160, BigInteger> OnNumberChanged;
+
+        public static bool ChangeNumber(BigInteger positiveNumber)
+        {
+            if (positiveNumber < 0)
+            {
+                throw new Exception("Only positive numbers are allowed.");
+            }
+
+            ContractStorage.Put(Tx.Sender, positiveNumber);
+            OnNumberChanged(Tx.Sender, positiveNumber);
+            return true;
+        }
+
+        [DisplayName("_deploy")]
+        public static void Deploy(object data, bool update)
+        {
+            if (!update)
+            {
+                ContractMetadata.Put("Owner", (ByteString) Tx.Sender);
+            }
+        }
+
+        public static ByteString GetNumber()
+        {
+            return ContractStorage.Get(Tx.Sender);
+        }
+
+        public static void UpdateContract(ByteString nefFile, string manifest)
+        {
+            ByteString owner = ContractMetadata.Get("Owner");
+            if (!Tx.Sender.Equals(owner))
+            {
+                throw new Exception("Only the contract owner can do this");
+            }
+            ContractManagement.Update(nefFile, manifest, null);
+        }
+
+        public static BigInteger TestArray(UInt160 from, ByteString[] tokenIDs)
+        {
+            return 1;
+        }
+
+        public static BigInteger TestObject(UInt160 from, object tokenIDs)
+        {
+            return 1;
+        }
+    }
+}
---
+++ contracts/MyArrays/test/MyArraysContractTests.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Neo.Assertions;
+using Neo.BlockchainToolkit;
+using Neo.BlockchainToolkit.Models;
+using Neo.BlockchainToolkit.SmartContract;
+using Neo.SmartContract;
+using Neo.VM;
+using Neo.VM.Types;
+using NeoTestHarness;
+using Xunit;
+
+namespace MyArraysTests
+{
+    [CheckpointPath("test/bin/checkpoints/contract-deployed.neoxp-checkpoint")]
+    public class MyArraysContractTests : IClassFixture<CheckpointFixture<MyArraysContractTests>>
+    {
+        readonly CheckpointFixture fixture;
+        readonly ExpressChain chain;
+
+        public MyArraysContractTests(CheckpointFixture<MyArraysContractTests> fixture)
+        {
+            this.fixture = fixture;
+            this.chain = fixture.FindChain("MyArraysTests.neo-express");
+        }
+
+        [Fact]
+        public void contract_owner_in_storage()
+        {
+            var settings = chain.GetProtocolSettings();
+            var owner = chain.GetDefaultAccount("owner").ToScriptHash(settings.AddressVersion);
+
+            using var snapshot = fixture.GetSnapshot();
+
+            // check to make sure contract owner stored in contract storage
+            var storages = snapshot.GetContractStorages<MyArraysContract>();
+            storages.Count().Should().Be(1);
+            storages.TryGetValue("MetadataOwner", out var item).Should().BeTrue();
+            item!.Should().Be(owner);
+        }
+
+        [Fact]
+        public void can_change_number()
+        {
+            var settings = chain.GetProtocolSettings();
+            var alice = chain.GetDefaultAccount("alice").ToScriptHash(settings.AddressVersion);
+
+            using var snapshot = fixture.GetSnapshot();
+
+            // ExecuteScript converts the provided expression(s) into a Neo script
+            // loads them into the engine and executes it 
+            using var engine = new TestApplicationEngine(snapshot, settings, alice);
+
+            engine.ExecuteScript<MyArraysContract>(c => c.changeNumber(42));
+
+            engine.State.Should().Be(VMState.HALT);
+            engine.ResultStack.Should().HaveCount(1);
+            engine.ResultStack.Peek(0).Should().BeTrue();
+
+            // ensure that notification is triggered
+            engine.Notifications.Should().HaveCount(1);
+            engine.Notifications[0].EventName.Should().Be("NumberChanged");
+            engine.Notifications[0].State[0].Should().BeEquivalentTo(alice);
+            engine.Notifications[0].State[1].Should().BeEquivalentTo(42);
+
+            // ensure correct storage item was created 
+            var storages = snapshot.GetContractStorages<MyArraysContract>();
+            var contractStorage = storages.StorageMap("MyArraysContract");
+            contractStorage.TryGetValue(alice, out var item).Should().BeTrue();
+            item!.Should().Be(42);
+        }
+
+        [Fact]
+        public void can_call_with_array()
+        {
+        var settings = chain.GetProtocolSettings();
+        var alice = chain.GetDefaultAccountScriptHash("alice");
+
+        using var snapshot = fixture.GetSnapshot();
+
+        // ExecuteScript converts the provided expression(s) into a Neo script
+        // loads them into the engine and executes it 
+        using var engine = new TestApplicationEngine(snapshot, settings, alice);
+
+        var tokenIdArray = new Array();
+        tokenIdArray.Add(new ByteString(Encoding.ASCII.GetBytes("")));
+        tokenIdArray.Add(new ByteString(Encoding.ASCII.GetBytes("")));
+
+        engine.ExecuteScript<MyArraysContract>(c => c.testArray(alice, tokenIdArray));
+
+        engine.State.Should().Be(VMState.HALT);
+        engine.ResultStack.Should().HaveCount(1);
+        engine.ResultStack.Peek(0).Should().BeTrue();
+        }
+
+        [Fact]
+        public void can_call_with_array_object()
+        {
+        var settings = chain.GetProtocolSettings();
+        var alice = chain.GetDefaultAccountScriptHash("alice");
+
+        using var snapshot = fixture.GetSnapshot();
+
+        // ExecuteScript converts the provided expression(s) into a Neo script
+        // loads them into the engine and executes it 
+        using var engine = new TestApplicationEngine(snapshot, settings, alice);
+
+        var tokenIdArray = new Array();
+        tokenIdArray.Add(new ByteString(Encoding.ASCII.GetBytes("")));
+        tokenIdArray.Add(new ByteString(Encoding.ASCII.GetBytes("")));
+
+        engine.ExecuteScript<MyArraysContract>(c => c.testObject(alice, tokenIdArray));
+
+        engine.State.Should().Be(VMState.HALT);
+        engine.ResultStack.Should().HaveCount(1);
+        engine.ResultStack.Peek(0).Should().BeTrue();
+        }
+    }
+}
